79f7b431abe9c569961500e114248e04f6803ca8
Harry Dudding <harry.dudding@ukaea.uk>
2024-03-08 09:45:13 +0000

    added code documentation, legend to example figures

--- TGLFNN_IBRANCH0_mode_analysis.py
+++ TGLFNN_IBRANCH0_mode_analysis.py
@@ -7,7 +7,7 @@ import pandas as pd
 # turbulent mode type. Reading functions adapted from previous scripts
 # by G. Szepesi.
 
-# H.G. Dudding, 25/01/2024
+# H.G. Dudding, 07/03/2024
 
 
 ''' Functions to read TGLF output files '''
@@ -49,14 +49,14 @@ def read_eigenvalue(tglf_directory, NMODES):
             strdat = strdat + line.split()
 
     # Number of columns in the reshaped data array:
-    ncol = 2 * NMODES # (gr, freq) for each eigenmode
+    ncol = 2 * NMODES # (gr, freq) for each mode
     # Convert strings into float array and reshape:
     data = convert_string_list_to_float_array(strdat, ncol)
     
     return data	
 
 # Gives the 5 fluxes: particle, energy, toroidal stress,
-# parallel stress, exchange, so "data[:,j]". Doesn't care about NMODES.
+# parallel stress, exchange, so "data[:,j]", for 0<=j<=4. Doesn't care about NMODES.
 # ---------------------------------------------------------------------
 def get_fluxes(tglf_directory, NKY, species, field, NFIELDS):
 	
@@ -102,7 +102,8 @@ def get_tglf_run_info(tglf_directory):
 	return NKY, NFIELDS, NMODES, NSPECIES
 
 # the most complicated one, in part due to there being different
-# line skips for modes than fields and species. "data[:,j]"
+# line skips for modes than fields and species. 
+# Like with get_fluxes, "data[:,j]" for 0<=j<=4.
 # ---------------------------------------------------------------------
 def get_weights(tglf_directory, NKY, species, field, mode, NFIELDS, NMODES):
 
@@ -127,10 +128,8 @@ def get_weights(tglf_directory, NKY, species, field, mode, NFIELDS, NMODES):
 	data = convert_string_list_to_float_array(strdat, ncol)
 	return data
 
-# Pretty simple here. 6 opening lines to skip. First column is 'vector',
-# then phi, A_par, B_par. But, arranged all n's per ky, and so must be 
-# split up, hence extra bit at the end. data_new[:,j] for different columns.
-# n is the mode number, 0 <= n <= NMODES - 1.
+# 6 opening lines to skip. First column is 'vector',
+# then phi, A_par, B_par. To access, data[:,j], 0<=j<=3.
 # ---------------------------------------------------------------------
 def get_field_spec(tglf_directory, NKY, NMODES, mode):
 	
@@ -235,13 +234,14 @@ def get_TGLF_weight_info(direc):
 		print('This script only deals with NFIELDS = 1.')
 	
 	all_weight_info = []
-	for m in range(2):
+	for m in range(2): # for the 2 modes
 		# get array of flux spectra for each species
 		species_weights = []
 		for species in range(NSPECIES):
-			type_weights = []
+			type_weights = [] # 'types' being particle, energy, etc
 			for j in range(5):
 				QL_weight_data = get_weights(direc, NKY, species, 0, m, NFIELDS, NMODES)[:,j]
+				# '0' here is the field phi, so electrostatic
 				type_weights.append(QL_weight_data)
 			species_weights.append(type_weights)
 		species_weights = np.array(species_weights)
@@ -249,10 +249,10 @@ def get_TGLF_weight_info(direc):
 	all_weight_info = np.array(all_weight_info)
 	
 	# indices:
-	# m = 0: electron mode
-	# m = 1: ion mode
+	# m = 0: electron frequency direction mode
+	# m = 1: ion frequency direction mode
 	# to get weight spectra for mode m, species s,
-	# for flux type k, : all_flux_info[m, s, k]
+	# for flux type k, : all_weight_info[m, s, k]
 	
 	# flux types: 0 particle
 	#			  1 energy
@@ -275,13 +275,25 @@ def get_growth_rates_and_frequencies(direc):
 	eigenvalue_data = read_eigenvalue(direc, NMODES)
 	# slice via
 	# eigenvalue_data[:, g]
-	# where g = 0: electron mode growth rate
-	#         = 1: electron mode frequency
-	#         = 2: ion mode growth rate
-	#         = 3: ion mode frequency
+	# where g = 0: electron frequency direction mode growth rate
+	#         = 1: electron frequency direction mode frequency
+	#         = 2: ion mode frequency direction growth rate
+	#         = 3: ion mode frequency direction frequency
 	# The ':' is because it's a spectrum of length len(kys).
 	return kys, eigenvalue_data
 	
+
+''' Two main things we're interested in analysing are 'all_weight_info'
+from 'get_TGLF_weight_info' and 'eigenvalue_data' from 'get_growth_rates_and_frequencies'.
+
+The labelling of 'electron frequency mode direction' and 'ion frequency mode direction'
+refers to the sign of the real frequency of the instability (whether its positive
+or negative, to help with mode identification), and shouldn't be confused
+with having species dependence. In TGLF, electron direction is taken to be positive
+and ion direction is taken to be negative. One can loosely think of 'ion direction' to be ITG
+and 'electron direction' to be TEM (for low ky). The rest of the labelling is explained
+at the end of the two functions above. '''
+
 
 for i in [7]:
 	tglf_directory = '/home/hdudding/work/PostDoc/DTP/tglfNN_project/tglf_standalone/GA_std_dgscan/' + str(i) + '/'
@@ -289,17 +301,51 @@ for i in [7]:
 	
 	test_function(tglf_directory)
 	
-	plt.plot(kys, eigen_dat[:, 0]) # electron mode growth rate
-	plt.plot(kys, eigen_dat[:, 2]) # ion mode growth rate
+	# Growth rate plot
+	plt.plot(kys, eigen_dat[:, 0], label = 'Elec. direc. ' + r'$\gamma_{k_y}$') # electron direction mode growth rate.
+																			   # should dominate at high ky!
+	plt.plot(kys, eigen_dat[:, 2], label = 'Ion direc. ' + r'$\gamma_{k_y}$') # ion direction mode growth rate
+	plt.xlabel(r'$k_y \rho_{\mathrm{unit}}$')
+	plt.ylabel(r'$\gamma_{k_y} \left(a / c_s\right)$')
+	plt.title('Growth rates')
+	plt.xscale('log')
+	plt.yscale('log')
+	plt.legend()
+	plt.grid()
 	plt.show()
 	
+	# Frequency plot
+	plt.plot(kys, eigen_dat[:, 1], label = 'Elec. direc. ' + r'$\omega_{k_y}$') # electron direction mode frequency,
+																			    # should be +ve
+	plt.plot(kys, eigen_dat[:, 3], label = 'Ion direc. ' + r'$\omega_{k_y}$') # ion direction mode frequency, should be -ve
+	plt.xlabel(r'$k_y \rho_{\mathrm{unit}}$')
+	plt.ylabel(r'$\omega_{k_y} \left(a / c_s\right)$')
+	plt.title('Frequencies')
+	plt.xscale('log')
+	plt.legend()
+	plt.grid()
+	plt.show()
+	
+	
+	# Weight plots
+	flux_types = ['Particle', 'Energy', 'Toroidal stress', 
+					'Parallel stress', 'Exchange']
+	species_labels = ['Electron', 'Ion']
+	mode_labels = ['Elec. direc', 'Ion direc.']
+	 
 	all_weight_info = get_TGLF_weight_info(tglf_directory)
 	NKY, NFIELDS, NMODES, NSPECIES = get_tglf_run_info(tglf_directory)
-	for mod in range(2):
-		for spec in range(2):
-			plt.plot(kys, get_fluxes(tglf_directory, NKY, spec, 0, NFIELDS))
-			plt.plot(kys, get_field_spec(tglf_directory, NKY, NMODES, mod))
-			plt.plot(kys, all_weight_info[mod, spec, 1])
-			plt.plot(kys, get_weights(tglf_directory, NKY, spec, 0, mod, NFIELDS, NMODES)[:,1], linestyle = '--')
+	
+	flux_type = 1 # energy, in this example
+	for mod in range(2): # mod = 0: electron direction mode, mod = 1: ion direction mode
+		for spec in range(2): # spec = 0: electron species, spec = 1: ion species
+			plt.plot(kys, all_weight_info[mod, spec, flux_type], label = str(mode_labels[mod]) + ' mode, ' + str(species_labels[spec]) + ' species '
+						+ str(flux_types[flux_type]) + ' weights')
+		plt.title(str(flux_types[flux_type]) + ' weights')
+		plt.xlabel(r'$k_y \rho_{\mathrm{unit}}$')
+		plt.ylabel(r'$W_{k_y} / W_{\mathrm{ref}}$')
+		plt.xscale('log')
+		plt.grid()
+		plt.legend()
 		plt.show()
 
